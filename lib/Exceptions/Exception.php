673e814fe442a619e33a1c3af23cf325b1ca0584
Maxom Sokolovsky <sokolovsky@worksolutions.ru>
2015-09-13 14:33:09 +0300

    Определение классов исключений

---
+++ lib/Exceptions/Exception.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WS\Tests\Exceptions;
+/**
+ * @author Maxim Sokolovsky <sokolovsky@worksolutions.ru>
+ */
+
+class Exception extends \Exception {
+}
---
+++ lib/Exceptions/RunTestException.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Maxim Sokolovsky <sokolovsky@worksolutions.ru>
+ */
+
+namespace WS\Tests\Exceptions;
+
+class RunTestException extends Exception {
+}
