11adddf336f76bbc3c92dc482ebab75f8451a8f8
Max Sokolovsky <sokolovsky@worksolutions.ru>
2015-09-23 17:59:05 +0300

    Базовый класс автотестов. Добавление мотодов именования

--- lib/Run/AutoTest.php
+++ lib/Run/AutoTest.php
@@ -5,5 +5,23 @@
 
 namespace WS\Tests\Run;
 
-class AutoTest extends Test {
+abstract class AutoTest extends Test {
+
+    static protected function generateSentence($word, $extra = array()) {
+        $extra = (array) $extra;
+        $words = explode(' ', preg_replace('/([A-Z])/', ' $1', str_replace($extra, '', $word)));
+        array_walk($words, function (& $word) {
+            $word = ucfirst($word);
+        });
+        return implode(' ', $words);
+    }
+
+    public function getName() {
+        $class = get_class($this);
+        return self::generateSentence($class, 'Test');
+    }
+
+    public function getTestName($methodName) {
+        return $this->getName().'. '.self::generateSentence($methodName, 'test');
+    }
 }
